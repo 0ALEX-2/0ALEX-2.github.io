91d262bda3fbe092166fa21db6dd8cc696473fb5
MD Sakil Ahmed <sakilahmed965@gmail.com>
2022-12-19 18:13:13 +0530

    yoox project added

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import "./projects.css"
 import pharmeasy from "../photos/pharmeasy.JPG"
+import yoox from "../photos/yoox.JPG"
 import { AiFillHtml5, } from "react-icons/ai";
-import { IoLogoCss3,IoLogoJavascript,IoLogoGithub } from "react-icons/io";
+import { FaReact } from "react-icons/fa";
+import { IoLogoCss3,IoLogoJavascript,IoLogoGithub} from "react-icons/io";
 
 const Projects = () => {
   return (
     <div id="projects" className='projects'>
       <h1>Projects</h1>
+
+
   <div className='pharmeasy' data-aos="fade-up"
     data-aos-duration="1000">
     <div className='banner'> <img src={pharmeasy} alt="banner" /></div>
@@ -24,6 +28,28 @@ const Projects = () => {
    <div className='buttns' >
        <button><a href="https://enchanting-bonbon-ad5692.netlify.app/" target="_blank">DEMO</a></button>
        <button><a href="https://github.com/nitinkrs/ready-lock-9044" target="_blank"><IoLogoGithub/></a></button>
+   </div>
+    </div>
+  </div>
+  
+
+  <div className='pharmeasy' data-aos="fade-up"
+    data-aos-duration="1000">
+    <div className='banner'> <img src={yoox} alt="banner" /></div>
+    <div className='aboutSec'>
+    <h3>This is a clone of Yoox.com. It is an e-commerce website, they sells Clothes, Shoes and various beauty products. This is a collaborative project. </h3> 
+    <div className='techUsed'>
+      <h2>Tech Used</h2>
+      <div className='icons' data-aos="zoom-in">
+           <AiFillHtml5/>
+           <IoLogoCss3/>
+           <IoLogoJavascript/>
+           <FaReact/>
+      </div>
+    </div>
+   <div className='buttns' >
+       <button><a href="https://luminous-sprinkles-3af9e6.netlify.app/" target="_blank">DEMO</a></button>
+       <button><a href="https://github.com/saikhmirsat/energized-rock-3853" target="_blank"><IoLogoGithub/></a></button>
    </div>
     </div>
   </div>
